41dc2ba8f433b131e8f500aafeea73f9bfda2949
emmanuelnoel80770-cyber <emmanuel.noel80770@gmail.com>
2025-12-29 22:21:48 +0100

    Update threads.py

---
+++ cogs/threads.py
@@ -0,0 +1,14 @@
+# Bot discord par manu_8001
+# Copyright (c) 2025 tous droits réservés
+
+# Importation des bibliothèques
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+intents.message_content = True
+
+class threads(commands.Cog):
+    def __init__(self, bot):
+        
