438a5b463e794e8afddb50c0dbed9ca0b25e11ee
emmanuelnoel80770-cyber <emmanuel.noel80770@gmail.com>
2025-12-29 21:06:44 +0100

    Create system_message.py

---
+++ cogs/system_message.py
@@ -0,0 +1,19 @@
+# Bot discord par manu_8001
+# Copyright (c) 2025 tous droits réservés
+
+# Importation des bibliothèques
+import os
+import discord
+
+from discord.ext import commands
+
+class SystemMessages(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    # Fonction qui affiche un message de validation.
+    # Paramètres: self = object
+    #             ctx = array(title, message, guild_id, channel_id, embed)
+    async def ValidMesage(self, ctx):
+        
