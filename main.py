0b7f2fd07512897291af1387908fe67025c1063d
emmanuelnoel80770-cyber <emmanuel.noel80770@gmail.com>
2025-12-29 20:47:15 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# Bot discord par manu_8001
+# Copyright (c) 2025 tous droits réservés
+
+# Importation des bibliothèques
+import os
+import discord
+
+from discord.ext import commands
+
+# On extrait le token du bot
+token = os.environ['Token_Bot_Discord']
+
+class BotDiscord(commands.Bot):
+    async def setup_hook(self):
+        for extension in ['']:
+            await self.load_extension(f'cogs.{extension}')
+
+intents = discord.Intents.all()
+bot = BotDiscord(command_prefix="!", intents=intents)
+
+bot.run(token=token)
