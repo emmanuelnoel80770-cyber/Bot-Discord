9fafb3da6adde4533400b124f2716abe72df39c4
emmanuelnoel80770-cyber <emmanuel.noel80770@gmail.com>
2025-12-29 22:14:38 +0100

    Update system_message.py

--- cogs/system_message.py
+++ cogs/system_message.py
@@ -2,3 +2,19 @@
 # Copyright (c) 2025 tous droits réservés
 
 # Importation des bibliothèques
+import os
+import discord
+from discord.ext import commands
+
+class system_message(commands.Cog):
+    def __init__(self, bot):
+       self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+       print(f'Connecté en tant que {self.bot.user}')
+
+
+async def setup(bot):
+   await bot.add_cog(system_message(bot))
+   print('Cog system_message chargé avec succès')
